f7af4d0e432063594bfe022013fcd3c657bf5132
mahesh-tippanu <102399228+mahesh-tippanu@users.noreply.github.com>
2022-04-29 12:12:06 +0500

    Update systembook.java

--- src/addressbook/systembook.java
+++ src/addressbook/systembook.java
@@ -1,34 +1,38 @@
 package addressbook;
-import javax.naming.Name;
 import java.util.ArrayList;
-public class systembook {
-    private String firstname,lastNmae,address,city,state,pincode,contactno,email;
-    public systembook(string fname,string lname,
-                      string address,string cityname,string state,string pincode,
-                      string contact_No,string email_id){
+import java.util.Scanner;
+public class addperson_details {
+    ArrayList<Contacts> addpersondetails = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+
+    public void addDetails() {
+        Contacts info = new Contacts();
+        System.out.println("Enter the First name: ");
+        info.setFirstName(sc.next());
+        System.out.println("Enter the last name: ");
+        info.setLastName(sc.next());
+        System.out.println("Enter Address: ");
+        info.setAddress(sc.next());
+        System.out.println("Enter the city");
+        info.setCity(sc.next());
+        System.out.println("Enter the State name: ");
+        info.setState(sc.next());
+        System.out.println("Enter the pincode: ");
+        info.setPincode(sc.next());
+        System.out.println("Enter the contact no: ");
+        info.setContact_no(sc.next());
+        System.out.println("Enter Email: ");
+        info.setEmail(sc.next());
+        addperson_details.addpersondetails(Contacts);
     }
-        this.firstname = fname;
-        this.lastname = lName;
-        this.address = address;
-        this.city = cityName;
-        this.state = state;
-        this.PipCode = PinCode;
-        this.contactNo = contact_No;
-        this.email = email_id;
+
+    public void Displayaddpersondetails() {
+        System.out.println(addperson_details);
     }
-    public String getfirstName()
-    {
-        return firstName;
-    }
-    public void setfirstName(String firstname) {
-        this.firstname = firstname;
-    }
-        public class systembookMain {
-        public static void main(String[] arys) {
-            System.out.println("Welcome to Address book");
-            contacts contacts_no = new contact_no("madhukar","tatiraju","gurdavara","viazg","AP","530040",""6303301745","madhukar@gmail.com"                                         
-            System.out.println(contact_no.getFirstName());
-            System.out.println(contacts.getEmail_id());       
-        }
+
+    public static void main(String[] args) {
+        systembook addressofperson1 = new systembook();
+        addressofperson1.()addperson_details;
+        addressofperson1.()Displayaddpersondetails;
     }
 }
